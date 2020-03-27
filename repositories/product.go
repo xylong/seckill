f0003a69426a4c311b9873bd0569363f6aa3e096
xyl <416319808@qq.com>
2020-03-27 23:41:33 +0800

    product curd and test

--- repositories/product.go
+++ repositories/product.go
@@ -1,15 +1,16 @@
 package repositories
 
 import (
+	"github.com/jmoiron/sqlx"
 	"seckill/datamodels"
 )
 
 type Product interface {
 	Insert(product *datamodels.Product) (id int64, err error)
-	Delete(id int64) (err error)
+	Delete(id int64) bool
 	Update(product *datamodels.Product) (err error)
 	Get(id int64) (product *datamodels.Product, err error)
-	Select() (products []*datamodels.Product, err error)
+	Select(ids []int64) (products []*datamodels.Product, err error)
 }
 
 func NewProduct() Product {
@@ -28,18 +29,47 @@ func (p *ProductManager) Insert(product *datamodels.Product) (id int64, err erro
 	return result.LastInsertId()
 }
 
-func (p *ProductManager) Delete(id int64) (err error) {
-	return
+func (p *ProductManager) Delete(id int64) bool {
+	sql := "DELETE FROM product WHERE id=?"
+	res, err := db.Exec(sql, id)
+	if err != nil {
+		return false
+	}
+	n, err := res.RowsAffected()
+	if err != nil || n == 0 {
+		return false
+	}
+	return true
 }
 
 func (p *ProductManager) Update(product *datamodels.Product) (err error) {
-	return
+	sql := "UPDATE PRODUCT SET name=?,number=?,image=?,url=? WHERE id=?"
+	res, err := db.Exec(sql, product.Name, product.Number, product.Url, product.Url, product.ID)
+	if err != nil {
+		return err
+	}
+	n, err := res.RowsAffected()
+	if err != nil || n == 0 {
+		return err
+	}
+	return nil
 }
 
 func (p *ProductManager) Get(id int64) (product *datamodels.Product, err error) {
+	product = &datamodels.Product{}
+	sql := "SELECT id,name,number,image,url FROM PRODUCT WHERE id=?"
+	err = db.Get(product, sql, id)
 	return
 }
 
-func (p *ProductManager) Select() (products []*datamodels.Product, err error) {
+func (p *ProductManager) Select(ids []int64) (products []*datamodels.Product, err error) {
+	sql, args, err := sqlx.In("SELECT id,name,number,image,url FROM PRODUCT WHERE id IN (?)", ids)
+	if err != nil {
+		return
+	}
+	err = db.Select(&products, sql, args...)
+	if err != nil {
+		return
+	}
 	return
 }
--- repositories/product_test.go
+++ repositories/product_test.go
@@ -19,3 +19,45 @@ func TestProductManager_Insert(t *testing.T) {
 	}
 	t.Logf("id:%d\n", id)
 }
+
+func TestProductManager_Delete(t *testing.T) {
+	p := NewProduct()
+	ok := p.Delete(6)
+	t.Logf("ok:%v\n", ok)
+}
+
+func TestProductManager_Update(t *testing.T) {
+	product := &datamodels.Product{
+		ID:     7,
+		Name:   "apple",
+		Number: 100,
+		Image:  "https://img12.360buyimg.com/n7/jfs/t1/86855/3/15583/138431/5e72575cE318939c3/8bca468b120af220.jpg",
+		Url:    "https://item.jd.com/100011979232.html",
+	}
+	p := NewProduct()
+	err := p.Update(product)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestProductManager_Get(t *testing.T) {
+	p := NewProduct()
+	product, err := p.Get(8)
+	if err != nil {
+		panic(err)
+	}
+	t.Logf("product:%v\n", product)
+}
+
+func TestProductManager_Select(t *testing.T) {
+	p := NewProduct()
+	ids := []int64{3, 4, 5}
+	products, err := p.Select(ids)
+	if err != nil {
+		panic(err)
+	}
+	for _, product := range products {
+		t.Logf("product:%#v\n", product)
+	}
+}
