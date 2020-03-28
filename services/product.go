29531c24bc9a73d539e249a1d57c9b1e8c1e55b8
xyl <416319808@qq.com>
2020-03-28 14:00:41 +0800

    'product'

---
+++ services/product.go
@@ -0,0 +1,38 @@
+package services
+
+import (
+	"seckill/datamodels"
+	"seckill/repositories"
+)
+
+type ProductService interface {
+	GetProductByID(int64) (*datamodels.Product, error)
+	//GetAllProduct()([]*datamodels.Product,error)
+	DeleteProductByID(int64)bool
+	InsertProduct(product *datamodels.Product)(int64,error)
+	UpdateProduct(product *datamodels.Product)error
+}
+
+func NewProductService() ProductService {
+	return &Ps{repositories.NewProduct()}
+}
+
+type Ps struct {
+	Product repositories.Product
+}
+
+func (p *Ps) GetProductByID(id int64) (*datamodels.Product, error) {
+	return p.Product.Get(id)
+}
+
+func (p *Ps) DeleteProductByID(id int64)bool {
+	return p.Product.Delete(id)
+}
+
+func (p *Ps) UpdateProduct(product *datamodels.Product)error {
+	return p.Product.Update(product)
+}
+
+func (p *Ps)  InsertProduct(product *datamodels.Product)(int64,error){
+	return p.Product.Insert(product)
+}
